8c7b0b3af5fc0776010873e6b828701662d68337
Mouse <937279156@qq.com>
2018-08-19 21:53:48 +0800

    Fixed Transformer

--- src/main/java/spigotlaunchwrapper/api/CorePlugin.java
+++ src/main/java/spigotlaunchwrapper/api/CorePlugin.java
@@ -4,8 +4,6 @@ import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
 
-import javax.xml.transform.Transformer;
-
 public interface CorePlugin {
 	
 	void acceptOptions(List<String> args, Path serverDir);
