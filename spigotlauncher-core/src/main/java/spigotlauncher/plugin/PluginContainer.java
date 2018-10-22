7e29451d23dd1f4655c1a497ee277b1f7969de23
Mouse <937279156@qq.com>
2018-10-22 23:30:35 +0800

    Added version, befores and afters

--- spigotlauncher-core/src/main/java/spigotlauncher/plugin/PluginContainer.java
+++ spigotlauncher-core/src/main/java/spigotlauncher/plugin/PluginContainer.java
@@ -1,51 +1,71 @@
 package spigotlauncher.plugin;
 
 import java.nio.file.Path;
+import java.util.List;
 
+import com.github.mouse0w0.version.ComparableVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import spigotlauncher.api.plugin.CorePlugin;
 
 public class PluginContainer {
-	
-	private final Path source;
-	private final String name;
-	private final Logger logger;
-	private final PluginClassLoader classLoader;
-	
-	private CorePlugin instance;
-	
-	public PluginContainer(Path source, String name) {
-		this.source = source;
-		this.name = name;
-		this.logger = LoggerFactory.getLogger(name);
-		this.classLoader = new PluginClassLoader(this, source);
-	}
-	
-	public Path getSource() {
-		return source;
-	}
-	
-	public String getName() {
-		return name;
-	}
 
-	public PluginClassLoader getClassLoader() {
-		return classLoader;
-	}
+    private final Path source;
+    private final String name;
+    private final ComparableVersion version;
+    private final Logger logger;
+    private final PluginClassLoader classLoader;
+    private final List<String> befores;
+    private final List<String> afters;
 
-	public CorePlugin getInstance() {
-		return instance;
-	}
+    private CorePlugin instance;
 
-	public Logger getLogger() {
-		return logger;
-	}
-	
-	void setInstance(CorePlugin instance) {
-		if(this.instance != null)
-			throw new UnsupportedOperationException("Instance has been set.");
-		this.instance = instance;
-	}
+    public PluginContainer(Path source, String name, ComparableVersion version, List<String> befores, List<String> afters) {
+        this.source = source;
+        this.name = name;
+        this.version = version;
+        this.befores = befores;
+        this.afters = afters;
+        this.logger = LoggerFactory.getLogger(name);
+        this.classLoader = new PluginClassLoader(this, source);
+    }
+
+    public Path getSource() {
+        return source;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ComparableVersion getVersion() {
+        return version;
+    }
+
+    public PluginClassLoader getClassLoader() {
+        return classLoader;
+    }
+
+    public List<String> getBefores() {
+        return befores;
+    }
+
+    public List<String> getAfters() {
+        return afters;
+    }
+
+    public CorePlugin getInstance() {
+        return instance;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    void setInstance(CorePlugin instance) {
+        if (this.instance != null)
+            throw new UnsupportedOperationException("Instance has been set.");
+        this.instance = instance;
+    }
 }
--- spigotlauncher-core/src/main/java/spigotlauncher/plugin/PluginLoader.java
+++ spigotlauncher-core/src/main/java/spigotlauncher/plugin/PluginLoader.java
@@ -7,19 +7,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
+import java.util.regex.Pattern;
 
+import com.github.mouse0w0.version.ComparableVersion;
 import spigotlauncher.Launch;
 import spigotlauncher.api.plugin.CorePlugin;
 
 public class PluginLoader {
 
+    private static final Pattern PLUGIN_NAME_PATTERN = Pattern.compile("[a-zA-Z0-9]+");
+    private static final Pattern PLUGINS_LIST_PATTERN = Pattern.compile("\\[([a-zA-Z0-9]+)?(([ ,]+)([a-zA-Z0-9]+))*\\]");
+
     private static final String COREPLUGIN_NAME_KEY = "CorePlugin-Name";
     private static final String COREPLUGIN_MAIN_KEY = "CorePlugin-Main";
+    private static final String COREPLUGIN_VERSION_KEY = "CorePlugin-Version";
+    private static final String COREPLUGIN_BEFORE_KEY = "CorePlugin-Before";
+    private static final String COREPLUGIN_AFTER_KEY = "CorePlugin-After";
 
     private final Path pluginDir;
 
@@ -28,9 +35,9 @@ public class PluginLoader {
     }
 
     public List<PluginContainer> loadAllCorePlugin() throws IOException {
-        final List<PluginContainer> containers = new ArrayList<>();
+        final List<PluginContainer> containers = new LinkedList<>();
 
-        if(!Files.exists(pluginDir)) {
+        if (!Files.exists(pluginDir)) {
             Launch.getLogger().warn("Plugins directory isn't exists. It will be create at {}", pluginDir.toAbsolutePath());
             Files.createDirectory(pluginDir);
         }
@@ -42,29 +49,58 @@ public class PluginLoader {
                 if (!file.getFileName().toString().endsWith(".jar"))
                     return FileVisitResult.CONTINUE;
 
+                String name = file.getFileName().toString();
+
                 try (JarFile jarFile = new JarFile(file.toString())) {
                     Manifest manifest = jarFile.getManifest();
+                    Attributes mainAttributes = manifest.getMainAttributes();
 
-                    String name = manifest.getMainAttributes().getValue(COREPLUGIN_NAME_KEY);
-                    String main = manifest.getMainAttributes().getValue(COREPLUGIN_MAIN_KEY);
-                    if (name == null || main == null)
-                        return FileVisitResult.CONTINUE;
+                    name = mainAttributes.getValue(COREPLUGIN_NAME_KEY);
+                    String main = mainAttributes.getValue(COREPLUGIN_MAIN_KEY);
+                    ComparableVersion version = new ComparableVersion(mainAttributes.getValue(COREPLUGIN_VERSION_KEY));
+                    List<String> befores = getList(mainAttributes, COREPLUGIN_BEFORE_KEY);
+                    List<String> afters = getList(mainAttributes, COREPLUGIN_AFTER_KEY);
+                    if (name == null || main == null || befores == null || afters == null)
+                        throw new PluginException("Illegal manifest. Cannot load core plugin.");
 
-                    try {
-                        PluginContainer container = loadCorePlugin(file, name, main);
-                        containers.add(container);
-                    } catch (PluginException e) {
-                        Launch.getLogger().warn(e.getMessage(), e);
-                    }
-                    return FileVisitResult.CONTINUE;
+                    PluginContainer container = loadCorePlugin(file, name, version, main, befores, afters);
+                    containers.add(container);
+                } catch (Exception e) {
+                    Launch.getLogger().warn(String.format("Cannot load core plugin: %s.", name), e);
                 }
+
+                return FileVisitResult.CONTINUE;
             }
         });
+
+        containers.sort((o1, o2) -> {
+            if(o1.getBefores().contains(o2.getName()) || o2.getAfters().contains(o1.getName()))
+                return 1;
+            if(o1.getAfters().contains(o1.getName()) || o2.getBefores().contains(o1.getName()))
+                return -1;
+            return 0;
+        });
+
         return containers;
     }
 
-    public PluginContainer loadCorePlugin(Path source, String name, String main) {
-        PluginContainer container = new PluginContainer(source, name);
+
+    private List<String> getList(Attributes attributes, String key) {
+        if (!attributes.containsKey(key))
+            return Collections.emptyList();
+
+        String value = attributes.getValue(key);
+        if (PLUGIN_NAME_PATTERN.matcher(value).matches()) {
+            return Collections.singletonList(value);
+        } else if (PLUGINS_LIST_PATTERN.matcher(value).matches()) {
+            return Arrays.asList(value.split("[ ,]+"));
+        } else {
+            return null;
+        }
+    }
+
+    public PluginContainer loadCorePlugin(Path source, String name, ComparableVersion version, String main, List<String> befores, List<String> afters) {
+        PluginContainer container = new PluginContainer(source, name, version, befores, afters);
         try {
             Class<?> mainClass = container.getClassLoader().loadClass(main);
             if (!CorePlugin.class.isAssignableFrom(mainClass))
